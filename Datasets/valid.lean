49cf48e79257ea7cbb5a925cd31491dce31ae454
story <lsy1538756437@126.com>
2025-03-30 17:31:59 -0400

    build2

--- Datasets/valid.lean
+++ Datasets/valid.lean
@@ -1,10 +1,3 @@
-/-
-Copyright (c) 2021 OpenAI. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kunhao Zheng, Stanislas Polu, David Renshaw, OpenAI GPT-f
-
-! This file was ported from Lean 3 source module valid and edited by Kaiyu Yang.
--/
 import Datasets.Minif2fimport
 
 
